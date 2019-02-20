fa07ddabd87ce3f3bb493cedeb76553db7e5e1d6
Jose Miguel Montanana <jmmontanana@gmail.com>
2019-02-21 00:25:52 +0100

    Update compile.sh

--- app_to_be_executed/compile.sh
+++ app_to_be_executed/compile.sh
@@ -2,11 +2,12 @@
 
 make clean;
 
-cp ../src/util/src/mf_util.h src;
-cp ../src/api/src/mf_api.h src;
-cp ../src/publisher/src/publisher.h src;
-cp ../src/parser/src/mf_parser.h src;
-cp ../src/core/mf_debug.h src;
+#!! please update with the correct path where you have the Mf_api
+cp ../Monitoring_client/src/util/src/mf_util.h src;
+cp ../Monitoring_client/src/api/src/mf_api.h src;
+cp ../Monitoring_client/src/publisher/src/publisher.h src;
+cp ../Monitoring_client/src/parser/src/mf_parser.h src;
+cp ../Monitoring_client/src/core/mf_debug.h src;
 
 #The compilation process:
 make VERBOSE=1 --makefile=./Makefile
